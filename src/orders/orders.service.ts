637330fb037779457547aad7159f1dc585831aff
BrunoMartino <bruno_g_martins@outlook.com>
2025-04-22 16:03:14 -0300

    feat:create Order Presenter, some minor fixes in Assets api

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -3,6 +3,7 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { Order, OrderStatus } from './entities/order.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { Asset } from 'src/assets/entities/asset.entity';
 
 @Injectable()
 export class OrdersService {
@@ -20,7 +21,10 @@ export class OrdersService {
   }
 
   findAll(filter: { walletId: string }) {
-    return this.orderSchema.find({ wallet: filter.walletId });
+    return this.orderSchema
+      .find({ wallet: filter.walletId })
+      .populate('asset') as Promise<(Order & { asset: Asset })[]>;
+    //.populate(['asset', 'trade']);
   }
 
   findOne(id: string) {
